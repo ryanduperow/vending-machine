2c40db51cfe002d6510b7d1403a988a972bc3485
Robert Kaiser <kaiserrj@gmail.com>
2021-10-10 16:25:21 -0400

    more tests

--- dotnet/CapstoneTests/AccountingTests.cs
+++ dotnet/CapstoneTests/AccountingTests.cs
@@ -23,5 +23,33 @@ namespace CapstoneTests
             Assert.AreEqual(expected, actual);
 
         }
+        [TestMethod]
+        public void PurchaseItemTest()
+        {
+            Capstone.Accounting ac = new Capstone.Accounting();
+
+            decimal balance = 8;
+            decimal price = 1.50M;
+
+            decimal expected = 6.50M;
+            decimal actual = ac.PurchaseItem(balance, price);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void CalculateTotalSalesTest()
+        {
+            Capstone.Accounting ac = new Capstone.Accounting();
+
+            decimal gross= 9;
+            decimal recent= 1.50M;
+
+            decimal expected = 10.50M;
+            decimal actual = ac.CalculateTotalSales(gross, recent);
+
+            Assert.AreEqual(expected, actual);
+
+        }
     }
 }
